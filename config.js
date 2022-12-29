c5d882113ca7553ce055a7e31307620376a74ff5
Jessehutapea123 <116166474+Jessehutapea123@users.noreply.github.com>
2022-12-29 08:28:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ let chalk = require('chalk')
 
 global.Owner = {
     // use the phone number with the country code, for example Indonesia '62' America '1' etc
-    '628818770766': { // 628818770766
-        name: 'Zevano',
+    '6285261896393': { // 6285261896393
+        name: 'Jesse',
         isReport: true, // if true this number will send if user use command /report or /join
         isCreator: true, // if true this number will send if user use command /owner or /creator
         isDev: true
@@ -15,10 +15,10 @@ global.ext = {
 	body: 'NayLa-B0TZ',
 	thum: 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 }
-global.mods = ['6283897390164'] // Want some help?
+global.mods = ['6285261896393'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.DeveloperMode = 'false' // true or false
-global.linkGC = ['https://chat.whatsapp.com/JgFOfu182li7kG6dlKcwzD']
+global.linkGC = ['-']
 global.packname = 'NayLa-B0TZ'
 global.author = 'Zevano'
 global.multiplier = 36 // The higher, The harder levelup
